95242c383dabec4659da34d7e067ed10fc4946d0
Lucas Vieira <lucasvs@id.uff.br>
2021-07-26 22:42:12 -0300

    Botão de filtragem adicionado

--- src/main/front-careforpet/js/scripts.js
+++ src/main/front-careforpet/js/scripts.js
@@ -3,5 +3,45 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
-// This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Datepicker
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'right',
+        locale: {
+            "format": "DD/MM/YYYY",
+            "separator": " - ",
+            "applyLabel": "Aplicar",
+            "cancelLabel": "Cancelar",
+            "fromLabel": "De",
+            "toLabel": "Até",
+            "customRangeLabel": "Custom",
+            "daysOfWeek": [
+                "Dom",
+                "Seg",
+                "Ter",
+                "Qua",
+                "Qui",
+                "Sex",
+                "Sáb"
+            ],
+            "monthNames": [
+                "Janeiro",
+                "Fevereiro",
+                "Março",
+                "Abril",
+                "Maio",
+                "Junho",
+                "Julho",
+                "Agosto",
+                "Setembro",
+                "Outubro",
+                "Novembro",
+                "Dezembro"
+            ],
+        }
+    }, function(start, end, label) {
+        console.log("Uma nova escolha de datas foi feita: " + start.format('YYYY-MM-DD') + ' até ' + end.format('YYYY-MM-DD'));
+    });
+});
