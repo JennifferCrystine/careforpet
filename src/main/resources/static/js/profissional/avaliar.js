03332d3cf8c6fd42eab7c01fd881d79c44e839a4
Lucas Vieira <lucasvs@id.uff.br>
2021-09-04 20:23:29 -0300

    Página de avaliação alterada e calendário alterado para data em pt

--- src/main/resources/static/js/profissional/avaliar.js
+++ src/main/resources/static/js/profissional/avaliar.js
@@ -1,6 +1,30 @@
-$(document).ready(function(){
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+const ratingResult = document.querySelector(".rating__result");
 
-    $("input[type='radio']").click(function(){
-        var sim = $("input[type='radio']:checked").val();
-//alert(sim);
-        if (sim<3) { $('.myratings').css('color','red'); $(".myratings").text(sim); }else{ $('.myratings').css('color','green'); $(".myratings").text(sim); } }); });
+printRatingResult(ratingResult);
+
+function executeRating(stars, result) {
+    const starClassActive = "rating__star fas fa-star";
+    const starClassUnactive = "rating__star far fa-star";
+    const starsLength = stars.length;
+    let i;
+    stars.map((star) => {
+        star.onclick = () => {
+            i = stars.indexOf(star);
+
+            if (star.className.indexOf(starClassUnactive) !== -1) {
+                printRatingResult(result, i + 1);
+                for (i; i >= 0; --i) stars[i].className = starClassActive;
+            } else {
+                printRatingResult(result, i);
+                for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+            }
+        };
+    });
+}
+
+function printRatingResult(result, num = 0) {
+    result.textContent = `${num}/5`;
+}
+
+executeRating(ratingStars, ratingResult);
